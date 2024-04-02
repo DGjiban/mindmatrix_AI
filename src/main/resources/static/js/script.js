e4ad63940e66d6016a1b82a42fd2b95921081bfa
Alpa204 <77805527+Alpa204@users.noreply.github.com>
2024-04-02 19:32:17 -0400

    commit lang,soc api, timer

--- src/main/resources/static/js/script.js
+++ src/main/resources/static/js/script.js
@@ -21,3 +21,19 @@ window.onload = function() {
 
     window.addEventListener('scroll', onScroll);
 };
+
+//timer
+const startingMinutes = 10;
+let time = startingMinutes * 60;
+
+const timerEl = document.getElementById('timer');
+setInterval(updateTimer, 1000);
+
+function updateTimer()
+{
+	const minutes = Math.floor(time / 60);
+	let seconds = time % 60;
+	timerEl.innerHTML = `${minutes}:${seconds}`;
+	
+	time--;
+}
